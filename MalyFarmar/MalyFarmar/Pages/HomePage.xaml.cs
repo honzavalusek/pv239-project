a4e18129c869bcd6b100c7c353b2c7f7d037c876
Ondřej Kříž <xkrizo05@stud.fit.vutbr.cz>
2025-05-08 09:35:09 +0200

    fully integrated viewmodel approach

--- MalyFarmar/MalyFarmar/Pages/HomePage.xaml.cs
+++ MalyFarmar/MalyFarmar/Pages/HomePage.xaml.cs
@@ -14,11 +14,13 @@ public partial class HomePage : ContentPage
 
     private async void OnCounterClicked(object sender, EventArgs e)
     {
-        var response = await _client.GetOrdersAsync(2);
+        var user = _client.GetUserAsync(Preferences.Default.Get("CurrentUserId", 0)).Result.FullName;
 
-        var user = SecureStorage.Default.GetAsync("CurrentUserId").Result;
+        var response = await _client.GetOrdersAsync(Preferences.Default.Get("CurrentUserId", 0));
+        
+        var orders = response.Orders.Count;
 
-        CounterBtn.Text = $"hello {user}, User 2 has {response.Orders.Count} orders";
+        CounterBtn.Text = $"hello {user}, you have {orders} orders";
 
         SemanticScreenReader.Announce(CounterBtn.Text);
     }
