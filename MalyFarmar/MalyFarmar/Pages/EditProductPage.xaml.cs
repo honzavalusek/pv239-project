f1c5268487d44b5eaa7c381aadccd02bfa1c2e8f
Ondřej Kříž <xkrizo05@stud.fit.vutbr.cz>
2025-05-08 21:35:06 +0200

    added edit product page

---
+++ MalyFarmar/MalyFarmar/Pages/EditProductPage.xaml.cs
@@ -0,0 +1,38 @@
+// Pages/EditProductPage.xaml.cs
+using MalyFarmar.ViewModels;
+using Microsoft.Maui.Controls;
+
+namespace MalyFarmar.Pages
+{
+    [QueryProperty(nameof(ProductId), "productId")]
+    public partial class EditProductPage : ContentPage
+    {
+        // This property will be set by the Shell navigation system
+        private int _productIdValue;
+        public int ProductId
+        {
+            get => _productIdValue;
+            set
+            {
+                _productIdValue = value;
+                // When ProductId is set on the page, pass it to the (already injected) ViewModel
+                if (BindingContext is EditProductViewModel viewModel)
+                {
+                    viewModel.ProductId = value;
+                }
+            }
+        }
+
+        public EditProductPage(EditProductViewModel viewModel) // ViewModel is injected by DI
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+
+            if (_productIdValue > 0) // If ProductId was set by QueryProperty very early
+            {
+                 viewModel.ProductId = _productIdValue;
+            }
+        }
+
+    }
+}
